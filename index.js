b44e91140eb9bf704bc09576210691320cb00c87
TessKi <kinnte@gmail.com>
2018-12-06 11:19:25 +0000

    set up index.js, installed some more packages

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
-
-const { port } = require('./config/environment');
-
 app.use(express.static(`${__dirname}/public`));
+
+const { port, dbUri } = require('./config/environment');
+
+const mongoose = require('mongoose');
+mongoose.connect(dbUri);
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
+const router = require('.config/router');
+app.use('/api', router);
 app.listen(port, () => console.log(`Express is listening on port ${port}`));
 
 module.exports = app;
