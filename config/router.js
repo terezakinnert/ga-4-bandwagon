b6bacbb2b8d878e091921d9fd65b363999149256
TessKi <kinnte@gmail.com>
2018-12-06 11:51:02 +0000

    added seeds

---
+++ config/router.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+const bandCtrl = require('../controllers/bandCtrl');
+
+// router.route('/')
---
+++ db/seeds.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+const { dbUri } = require('../config/environment');
+
+const Band = require('../models/band');
+const User = require('../models/user');
+const Instrument = require('../models/instrument');
+const Message = require('../models/message');
+
+const userIds = [
+  '5c090718a118163cfc161168',
+  '5c090724a118163cfc161169'
+];
+
+const bandData = [
+  {
+    name: 'Blues Brothers',
+    location: 'Chicago',
+    lookingForMembers: true,
+    lookingForInstrument: 'Drums'
+  },
+  {
+    name: 'Shellac',
+    location: 'Chicago',
+    lookingForMembers: false,
+    lookingForInstrument: 'Vocals'
+  }
+];
+
+const userData = [
+  {
+    _id: userIds[0],
+    username: 'Bob',
+    email: 'b@b',
+    password: 'pass'
+  }, {
+    _id: userIds[1],
+    username: 'Jake',
+    email: 'j@j',
+    password: 'pass'
+  }
+];
+
+const instrumentData = [
+  ['Drums', 'Vocals']
+];
+
+const messageData = [
+  {
+    from: userIds[0],
+    to: userIds[1],
+    content: 'Hi! You wanna jam?'
+  }, {
+    from: userIds[1],
+    to: userIds[0],
+    content: 'Hi, sure! Whereabouts are you?'
+  }
+];
+
+mongoose.connect(dbUri);
+Band.collection.drop();
+User.collection.drop();
+Instrument.collection.drop();
+Message.collection.drop();
+
+Band.create(bandData)
+  .then(bands => {
+    console.log(`Created ${bands.length} bands.`);
+    return User.create(userData);
+  })
+  .then(users => {
+    console.log(`Created ${users.length} users.`);
+    return Instrument.create(instrumentData);
+  })
+  .then(instruments => {
+    console.log(`Created ${instruments.length} instruments.`);
+    return Message.create(messageData);
+  })
+  .then(messages => {
+    console.log(`Created ${messages.length} messages.`);
+    mongoose.connection.close();
+  })
+  .catch(err => console.log(err));
