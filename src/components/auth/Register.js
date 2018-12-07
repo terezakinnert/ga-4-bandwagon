05f33579e28b09f12ba460dc762862f8add14c54
TessKi <kinnte@gmail.com>
2018-12-07 14:51:32 +0000

    this.state undefined

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import axios from 'axios';
-import { withRouter } from 'react-router-dom';
+// import { withRouter } from 'react-router-dom';
 
 import { saveToken } from '../../lib/auth';
-import { handleChange } from '../../lib/common';
+// import { handleChange } from '../../lib/common';
 
 class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.state.handleChange = handleChange.bind(this);
+    this.state.handleChange = this.handleChange.bind(this);
     this.state.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { target: {name, value} } = event;
+    this.setState({ [name]: value });
   }
 
   handleSubmit(event) {
     event.preventDefault();
     axios.post('/api/register', this.state)
       .then(result => {
+        console.log('this.state', this.state, '+ result', result);
         saveToken(result.data.token);
       })
       .then(() => this.props.history.push('/'))
@@ -24,23 +30,39 @@ class Register extends React.Component {
         this.props.history.replace('/login');
       });
   }
+  //
+  // handleSubmit(e) {
+  //     e.preventDefault();
+  //
+  //     axios.post('/api/register', this.state)
+  //       .then(res => {
+  //         saveToken(res.data.token);
+  //       })
+  //       .then(() => this.props.history.push('/burgers'))
+  //       .catch(() => {
+  //         this.props.history.replace('/login');
+  //       });
+  //   }
+
 
   render() {
     return(
       <div>
         <form onSubmit={this.handleSubmit}>
           <label>Username</label>
-          <input name="username" onChange={this.handleChange} />
+          <input name="username" type="text" onChange={this.handleChange} />
 
           <label>Email</label>
-          <input name="email" onChange={this.handleChange} />
+          <input name="email" type="email" onChange={this.handleChange} />
 
           <label>Password</label>
-          <input name="password" onChange={this.handleChange} />
+          <input name="password" type="password" onChange={this.handleChange} />
+
+          <button>Sign Up</button>
         </form>
       </div>
     );
   }
 }
 
-export default withRouter(Register);
+export default Register;
