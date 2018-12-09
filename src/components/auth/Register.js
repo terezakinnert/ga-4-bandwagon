702b9de4fa726a5d267684ba9d5e7cd4ce7858e8
TessKi <kinnte@gmail.com>
2018-12-09 20:38:23 +0000

    changed register form

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,8 +1,6 @@
 import React  from 'react';
 import axios from 'axios';
 
-// import handleChange from '../../lib/common';
-
 class Register extends React.Component {
   constructor(props) {
     super(props);
@@ -28,14 +26,39 @@ class Register extends React.Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <label>Username</label>
-          <input name="username" type="text" onChange={this.handleChange} value={this.state.username || ''} />
+          <label htmlFor="name">Username</label>
+          <input name="username" type="username" onChange={this.handleChange} value={this.state.username || ''} />
 
           <label>Email</label>
           <input name="email" type="email" onChange={this.handleChange} value={this.state.email || ''} />
 
           <label>Password</label>
           <input name="password" type="password" onChange={this.handleChange} value={this.state.password || ''} />
+
+          <label htmlFor="location">Location</label>
+          <input name="location" type="text" onChange={this.handleChange} value={this.state.location || ''} />
+
+          <span>Want to join a band?</span>
+          <div>
+            <input name="lookingForBands" type="radio" id="yes" onChange={this.handleChange} value="true" />
+            <label htmlFor="true">Yes</label>
+          </div>
+          <div>
+            <input name="lookingForBands" type="radio" id="no" onChange={this.handleChange} value="false" />
+            <label htmlFor="false">Not right now</label>
+          </div>
+
+          <label htmlFor="image">Image URL</label>
+          <input name="image" type="text" onChange={this.handleChange} value={this.state.image || ''} />
+
+          <label htmlFor="website">Your website address</label>
+          <input name="website" type="text" onChange={this.handleChange} value={this.state.website || ''} />
+
+          <label htmlFor="genres">What kind of music do you play?</label>
+          <input name="genres" type="text" onChange={this.handleChange} value={this.state.genres || ''} />
+
+          <label htmlFor="influences">Your influences:</label>
+          <input name="influences" type="text" onChange={this.handleChange} value={this.state.influences || ''} />
 
           <button>Sign Up</button>
         </form>
