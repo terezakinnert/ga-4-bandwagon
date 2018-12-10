cf34e9469de29803e978e1271bad63d004d1264f
TessKi <kinnte@gmail.com>
2018-12-10 09:57:05 +0000

    added secure route links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { deleteToken, isAuthenticated } from '../lib/auth';
 
 class Header extends React.Component {
   constructor(props) {
@@ -7,15 +8,21 @@ class Header extends React.Component {
     this.state = {};
   }
 
+  handleLogout() {
+    deleteToken();
+    this.props.history.push('/');
+  }
+
   render() {
     return(
       <nav>
         <Link to="/">Home</Link>
         <Link to="/bands">All Bands</Link>
-        <Link to="/bands/new">Add Your Band</Link>
-        <Link to="/users">All Musicians</Link>
-        <Link to="/register">Sign Up</Link>
-        <Link to="/login">Log In</Link>
+        {isAuthenticated() && <Link to="/bands/new">Add Your Band</Link>}
+        {isAuthenticated() && <Link to="/users">All Musicians</Link>}
+        {!isAuthenticated() && <Link to="/login">Log In</Link>}
+        {isAuthenticated() && <a onClick={this.handleLogout}>Log Out</a>}
+        {!isAuthenticated() && <Link to="/register">Sign Up</Link>}
       </nav>
     );
   }
